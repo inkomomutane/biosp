39f7459ae619c3ededa9158644414b8209b64a46
Nelson Mutane <nelsonmutane@gmail.com>
2021-08-21 21:53:21 +0200

    api login and logout created

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
+use App\Http\Requests\Api\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -11,67 +11,31 @@ use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+
+    /** Api logout
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+
+    public function logout(Request $request)
     {
-        //
+        if(auth()->check()){
+            $user = User::find(auth()->user()->uuid);
+            return  $user->tokens()->delete() ? response()->json('Logout feito com sucesso',200) : response()->json('Erro ao fazer logout',403);
+        }
+        return  response()->json('Erro ao fazer logout',403);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Models\User  $user
-     * @return \Illuminate\Http\Response
+     * Api login
+     * @param \App\Http\Requests\Api\LoginRequest $request
+     * @return array
      */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
-    }
-
 
     public function login(LoginRequest $request)
     {
         $user = User::where('email', $request->email)->first();
-
-
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['Email ou senha incorretas.'],
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login',[UserController::class,'login']);
+Route::post('/logout',[UserController::class,'logout'])->middleware('auth:sanctum');
+
