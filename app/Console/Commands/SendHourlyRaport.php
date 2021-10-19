60267b9bb6119dc7d74fccf33cfcf28fcb3fe6c9
Nelson Mutane <57417802+inkomomutane@users.noreply.github.com>
2021-10-20 01:36:52 +0200

    Hourly Data verification working

--- app/Console/Commands/SendHourlyRaport.php
+++ app/Console/Commands/SendHourlyRaport.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Benificiary;
 use Illuminate\Console\Command;
 use Paymentsds\MPesa\Client;
 use Illuminate\Support\Str;
@@ -39,6 +40,12 @@ class SendHourlyRaport extends Command
      */
     public function handle()
     {
+        $cp = Benificiary::whereIn('neighborhood_uuid', '9a81ea14-8896-4d24-b4d7-62888aa37911')->WhereDate('created_at', now()->day)->count();
+        $ma = Benificiary::whereIn('neighborhood_uuid', 'af1103d9-7240-483e-8721-6d32d04d127e')->WhereDate('created_at', now()->day)->count();
+        $maf = Benificiary::whereIn('neighborhood_uuid', 'c3eccf0a-150b-4417-9a56-63d9d7c5c7d2')->WhereDate('created_at', now()->day)->count();
+        $mu = Benificiary::whereIn('neighborhood_uuid', '2a26a8c8-12aa-41c2-a01f-e7959dbcee98')->WhereDate('created_at', now()->day)->count();
+        $in = Benificiary::whereIn('neighborhood_uuid', '61ed1d72-01fa-488c-a83f-e3a16a0c3e0d')->WhereDate('created_at', now()->day)->count();
+
         $client = new Client([
             'apiKey' => '83f7zvajuj5dp27aj9fsrcm1py6lqsr4',             // API Key
             'publicKey' => 'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmptSWqV7cGUUJJhUBxsMLonux24u+FoTlrb+4Kgc6092JIszmI1QUoMohaDDXSVueXx6IXwYGsjjWY32HGXj1iQhkALXfObJ4DqXn5h6E8y5/xQYNAyd5bpN5Z8r892B6toGzZQVB7qtebH4apDjmvTi5FGZVjVYxalyyQkj4uQbbRQjgCkubSi45Xl4CGtLqZztsKssWz3mcKncgTnq3DHGYYEYiKq0xIj100LGbnvNz20Sgqmw/cH+Bua4GJsWYLEqf/h/yiMgiBbxFxsnwZl0im5vXDlwKPw+QnO2fscDhxZFAwV06bgG0oEoWm9FnjMsfvwm0rUNYFlZ+TOtCEhmhtFp+Tsx9jPCuOd5h2emGdSKD8A6jtwhNa7oQ8RtLEEqwAn44orENa1ibOkxMiiiFpmmJkwgZPOG/zMCjXIrrhDWTDUOZaPx/lEQoInJoE2i43VN/HTGCCw8dKQAwg0jsEXau5ixD0GUothqvuX3B9taoeoFAIvUPEq35YulprMM7ThdKodSHvhnwKG82dCsodRwY428kg2xM/UjiTENog4B6zzZfPhMxFlOSFX4MnrqkAS+8Jamhy1GgoHkEMrsT5+/ofjCx0HjKbT5NuA2V/lmzgJLl3jIERadLzuTYnKGWxVJcGLkWXlEPYLbiaKzbJb2sYxt+Kt5OxQqC1MCAwEAAQ==',          // Public Key
@@ -46,16 +53,21 @@ class SendHourlyRaport extends Command
         ]);
         $paymentData = [
             'from' => '258847607095',       // input_CustomerMSISDN
-            'reference' => 'CP900MA457MAF478MU4874IN457'.Str::random(5),      // input_ThirdPartyReference
+            'reference' =>
+            'CP' . $cp .
+                'MA' . $ma .
+                'MAF' . $maf .
+                'MU' . $mu .
+                'IN' . $in . Str::random(5),      // input_ThirdPartyReference
             'transaction' => Str::random(12), // input_TransactionReference
-            'amount' => '5'             // input_Amount
+            'amount' => $cp  + $ma + $maf  + $mu + $in            // input_Amount
         ];
 
         try {
             $client->receive($paymentData);
             $this->info('Report sent successful.');
-         } catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             $this->info('Error sending report.');
-         }
+        }
     }
 }
