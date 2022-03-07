52945ea47d5353350366c6f17a2505ca0e190ccb
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-03-07 08:10:48 +0200

    Mail report updated o send last month query

--- app/Http/Controllers/DashbordController.php
+++ app/Http/Controllers/DashbordController.php
@@ -226,7 +226,7 @@ class DashbordController extends Controller
 
     public function thisMonthForMail(Neighborhood $bairro)
     {
-        return $this->importCollection(BenificiaryResource::collection(Benificiary::where('neighborhood_uuid', $bairro->uuid)->whereMonth('service_date', (now()->month))->get()), $bairro->name, true);
+        return $this->importCollection(BenificiaryResource::collection(Benificiary::where('neighborhood_uuid', $bairro->uuid)->whereMonth('service_date', (now()->month) - 1 )->get()), $bairro->name, true);
     }
 
 
